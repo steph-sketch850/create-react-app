8a75ccb2dee4e9e4835040e53c5015794718e0aa
steph-sketch850 <145380074+steph-sketch850@users.noreply.github.com>
2024-08-21 19:55:37 -0400

    Fixed error message for showTemperature variable

--- src/Weather.js
+++ src/Weather.js
@@ -4,12 +4,11 @@ import axios from "axios";
 export default function Temperature(props) {
   const [temperature, setTemperature] = useState(null);
 
-  function showTemperature(response) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+  axios.get(url).then((response) => {
     setTemperature(response.data.main.temp);
+  });
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
-    axios.get(url).then(showTemperature);
-  }
   if (temperature) {
     return (
       <p>
